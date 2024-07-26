b6da3cdb0ce29b6fc3f5febd481360676b18c3dd
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-26 20:41:57 +0530

    Add project files.

---
+++ InheritenceProject/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceProject
+{
+    internal class Class1
+    {
+        public Class1(int i) {
+            Console.WriteLine("Class 1 constructor  is callled" + i);
+        } 
+        public void Test1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method 2");
+        }
+        static void Main()
+        {
+           // Class1 p;           // p is  variable of class 1
+            
+                Console.ReadLine();
+
+        }
+        }
+}
---
+++ InheritenceProject/Class2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceProject
+{
+    class Class2 : Class1
+    {
+       public Class2(int a) :base(a)
+        {
+            Console.WriteLine("Class 2 constructor  is callled");
+        }
+        public void Test3()
+        {
+            Console.WriteLine("Method 3");
+
+        }
+        static void Main()
+        {   
+            Class2 c = new Class2(50);    
+         //   Object obj  = new object();
+        //    Console.WriteLine(obj.GetType());
+        //    Class1 p = new Class1();
+        //    Console.WriteLine(p.GetType());
+        //    Class2 c = new Class2();
+        //    Console.WriteLine(c.GetType());
+
+           
+           // Class1 p;
+
+//            Class2 c = new Class2();  // c is a instance of class 2
+  //          p = c;
+    //        p.Test1();
+      //      p.Test2();  // p is a refrence of parent class by using child class instance
+        //    c.Test1();
+          //  c.Test2();
+           //  c.Test3();
+            
+            Console.ReadLine();
+
+        }
+    }
+}
